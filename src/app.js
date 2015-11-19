eeb2b9068b660868aa3192c505b10823b4ed3914
bttf <adnanchowdhury88@gmail.com>
2015-11-18 20:53:41 -0800

    integrate socket.io

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const path = require('path');
 const engines = require('consolidate');
 const router = require('./router');
+const socketHandler = require('./socketHandler');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', engines.handlebars);
 
 app.use(router);
+io.on('connection', socketHandler);
 
 http.listen(3000, () => {
-  console.log('listening on *:3000 test');
+  console.log('listening on *:3000');
 });
